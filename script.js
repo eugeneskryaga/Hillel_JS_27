02bfea742c3c11db3e34b0413e27a1eaca3e0cf1
Eugene <zhenyaskryaga@gmail.com>
2026-01-08 15:41:23 +0200

    homework 27

--- script.js
+++ script.js
@@ -30,21 +30,56 @@ const root = document.querySelector("#root");
 const title = document.createElement("h1");
 title.textContent = "Список книг";
 
-root.append(title);
+const markup = document.createElement("div");
+markup.style.display = "flex";
+markup.style.gap = "40px";
+root.append(markup);
+
+const bookListContainer = document.createElement("div");
+const border = "2px solid black";
+const padding = "20px";
+bookListContainer.style.border = border;
+bookListContainer.style.padding = padding;
+markup.append(bookListContainer);
+bookListContainer.append(title);
+
+const bookDescContainer = document.createElement("div");
+markup.append(bookDescContainer);
+
+function showDetails(book) {
+  bookDescContainer.innerHTML = "";
+
+  bookDescContainer.style.border = border;
+  bookDescContainer.style.padding = padding;
+
+  const title = document.createElement("h2");
+  title.textContent = book.title;
+
+  const author = document.createElement("p");
+  author.innerHTML = `<strong>Автор</strong> ${book.author}`;
+
+  const year = document.createElement("p");
+  year.innerHTML = `<strong>Рік видання</strong> ${book.year}`;
+
+  const desc = document.createElement("p");
+  desc.innerHTML = `<strong>Опис</strong> ${book.desc}`;
+
+  bookDescContainer.append(title, author, year, desc);
+}
 
 const bookList = document.createElement("ul");
-root.append(bookList);
+bookListContainer.append(bookList);
 
 books.forEach(book => {
-  const bookListItem = document.createElement("li");
-  bookListItem.textContent = book.title + " ";
-  bookList.append(bookListItem);
-});
+  const listItem = document.createElement("li");
+  listItem.style.marginBottom = "10px";
 
-const bookListItems = bookList.querySelectorAll("li");
+  const bookTitle = book.title + " ";
 
-bookListItems.forEach(li => {
-  const viewDetailsBtn = document.createElement("button");
-  viewDetailsBtn.textContent = "View Details";
-  li.append(viewDetailsBtn);
+  const button = document.createElement("button");
+  button.textContent = "View Details";
+  button.addEventListener("click", () => showDetails(book));
+
+  listItem.append(bookTitle, button);
+  bookList.appendChild(listItem);
 });
