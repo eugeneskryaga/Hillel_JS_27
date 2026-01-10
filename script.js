b4a57af2372c81659cb2039a02850db2a8e72d34
Eugene <zhenyaskryaga@gmail.com>
2026-01-11 01:13:45 +0200

    homework 28

--- script.js
+++ script.js
@@ -39,124 +39,92 @@ const border = "2px solid black";
 const padding = "20px";
 
 const bookListContainer = document.createElement("div");
-
 bookListContainer.style.border = border;
 bookListContainer.style.padding = padding;
 markup.append(bookListContainer);
 bookListContainer.append(title);
 
-const addBook = document.createElement("button");
-addBook.textContent = "Add Book";
+const bookList = document.createElement("ul");
+bookListContainer.append(bookList);
+
+const addBookBtn = document.createElement("button");
+addBookBtn.textContent = "Add Book";
+bookListContainer.append(addBookBtn);
 
 const bookDescContainer = document.createElement("div");
+bookDescContainer.style.maxWidth = "50%";
 markup.append(bookDescContainer);
+
+function showBookList() {
+  bookList.innerHTML = "";
+
+  books.forEach(book => {
+    const listItem = document.createElement("li");
+    listItem.style.marginBottom = "10px";
+
+    const bookTitle = book.title + " ";
+
+    const button = document.createElement("button");
+    button.textContent = "View Details";
+    button.addEventListener("click", () => showDetails(book));
+
+    listItem.append(bookTitle, button);
+    bookList.appendChild(listItem);
+  });
+}
 
 function showDetails(book) {
   bookDescContainer.innerHTML = "";
-
   bookDescContainer.style.border = border;
   bookDescContainer.style.padding = padding;
 
-  const title = document.createElement("h2");
-  title.textContent = book.title;
-
-  const author = document.createElement("p");
-  author.innerHTML = `<strong>Автор</strong> ${book.author}`;
-
-  const year = document.createElement("p");
-  year.innerHTML = `<strong>Рік видання</strong> ${book.year}`;
-
-  const desc = document.createElement("p");
-  desc.innerHTML = `<strong>Опис</strong> ${book.description}`;
-
-  bookDescContainer.append(title, author, year, desc);
+  bookDescContainer.innerHTML = `
+    <h2>${book.title}</h2>
+    <p><strong>Автор:</strong> ${book.author}</p>
+    <p><strong>Рік видання:</strong> ${book.year}</p>
+    <p><strong>Опис:</strong> ${book.description}</p>
+  `;
 }
 
-function addDescription(books) {
+function addDescription() {
   bookDescContainer.innerHTML = "";
-
   bookDescContainer.style.border = border;
   bookDescContainer.style.padding = padding;
 
   const form = document.createElement("form");
   form.style.display = "flex";
   form.style.flexDirection = "column";
+  form.style.gap = "30px";
 
-  const margin = "30px";
-
-  const titleLabel = document.createElement("label");
-  titleLabel.textContent = "Enter title: ";
-  const inputTitle = document.createElement("input");
-  inputTitle.setAttribute("name", "title");
-  titleLabel.style.marginBottom = margin;
-  titleLabel.append(inputTitle);
-
-  const authorLabel = document.createElement("label");
-  authorLabel.textContent = "Enter author: ";
-  const inputAuthor = document.createElement("input");
-  inputAuthor.setAttribute("name", "author");
-  authorLabel.style.marginBottom = margin;
-  authorLabel.append(inputAuthor);
-
-  const yearLabel = document.createElement("label");
-  yearLabel.textContent = "Enter publishing year: ";
-  const inputYear = document.createElement("input");
-  inputYear.setAttribute("name", "year");
-  yearLabel.style.marginBottom = margin;
-  yearLabel.append(inputYear);
-
-  const descLabel = document.createElement("label");
-  descLabel.textContent = "Enter description: ";
-  const inputDesc = document.createElement("input");
-  inputDesc.setAttribute("name", "desc");
-  descLabel.style.marginBottom = margin;
-  descLabel.append(inputDesc);
-
-  const submit = document.createElement("button");
-  submit.textContent = "Submit";
-
-  form.append(titleLabel, authorLabel, yearLabel, descLabel, submit);
-  bookDescContainer.append(form);
+  form.innerHTML = `
+    <label>Title <input name="title"></label>
+    <label>Author <input name="author"></label>
+    <label>Year <input name="year"></label>
+    <label>Description <input name="desc"></label>
+    <button type="submit">Submit</button>
+  `;
 
   form.addEventListener("submit", e => {
     e.preventDefault();
 
     const data = new FormData(e.target);
 
-    const title = data.get("title");
-    const author = data.get("author");
-    const year = data.get("year");
-    const desc = data.get("desc");
-    const id = books.length + 1;
+    books.push({
+      id: books.length + 1,
+      title: data.get("title"),
+      author: data.get("author"),
+      year: data.get("year"),
+      description: data.get("desc"),
+    });
 
-    const book = {
-      id: id,
-      title: title,
-      author: author,
-      year: year,
-      description: desc,
-    };
-
-    books.push(book);
+    showBookList();
+    bookDescContainer.innerHTML = "";
+    bookDescContainer.style.border = "none";
   });
+
+  bookDescContainer.append(form);
 }
 
-const bookList = document.createElement("ul");
-bookListContainer.append(bookList);
+addBookBtn.addEventListener("click", addDescription);
 
-books.forEach(book => {
-  const listItem = document.createElement("li");
-  listItem.style.marginBottom = "10px";
-
-  const bookTitle = book.title + " ";
-
-  const button = document.createElement("button");
-  button.textContent = "View Details";
-  button.addEventListener("click", () => showDetails(book));
-
-  listItem.append(bookTitle, button);
-  bookList.appendChild(listItem);
-
-  bookListContainer.append(addBook);
-  addBook.addEventListener("click", () => addDescription(books));
-});
+showBookList();
